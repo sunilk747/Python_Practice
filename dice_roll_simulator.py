2808897d3e3b4da26151251fe3d6f93cf5d95d51
Collin Guarino <collin.guarino@gmail.com>
2014-08-07 21:02:15 -0400

    Fixed git issue with project not updating

--- dice_roll_simulator.py
+++ dice_roll_simulator.py
@@ -1,1 +1,30 @@
-__author__ = 'collinux'
+import sys
+from random import randrange
+
+def main():
+    showOptionsConfiguration()
+    while True:
+        followupInput = raw_input("Press \'r\' to reroll | \'c\' to re-configure | \'q\' to quit. ")
+        if 'r' in followupInput:
+            roll()
+        elif 'c' in followupInput:
+            showOptionsConfiguration()
+        elif 'q' in followupInput:
+            sys.exit(0)
+        else:
+            print "Please enter a valid command: "
+
+def showOptionsConfiguration():
+    global numDice
+    numDice = int(raw_input("How many dice would you like to roll at a time? "))
+    global numSides
+    numSides = int(raw_input("How many sides are on each dice? "))
+    roll()
+
+def roll():
+    numRolls = 0
+    while numRolls < numDice:
+        print str(randrange(1, numSides))
+        numRolls += 1
+
+main()
